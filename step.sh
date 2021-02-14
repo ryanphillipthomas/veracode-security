278e642aae9d537b8502f4f85cff7e995b3c962b
ryanphillipthomas <ryanphillipthomas@mac.com>
2021-02-14 09:56:17 -0500

    Remove additional shebang

--- step.sh
+++ step.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 
-#!/usr/bin/env bash
 echo "Uploading File: ${file_upload_path} for App ID: ${veracode_app_id}"
 java -jar $BITRISE_STEP_SOURCE_DIR/VeracodeJavaAPI.jar -vid ${veracode_api_id} -vkey ${veracode_api_secret} -action uploadfile -appid ${veracode_app_id} -filepath ${file_upload_path}
 
